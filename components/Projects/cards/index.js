57120d9851bf9adbf8234ae650145c27520f5c53
senthur <senthur@kuvera.in>
2022-08-27 23:11:18 +0530

    [Fixed all]

--- components/Projects/cards/index.js
+++ components/Projects/cards/index.js
@@ -1,15 +1,17 @@
 import styles from './card.module.css';
 const Cards =  ({title, defaultImg, handleClick})=>{
-    return <div className={styles.card} style={{
-            backgroundRepeat:'no-repeat',
-            backgroundImage: `url("${defaultImg}")`,
-            backgroundPosition: 'center',
-            backgroundSize: 'cover'
-        }} onClick={handleClick}>
-        <div className={styles.overlay}>
-            <div>{title}</div>
+    return <div className={styles.wrapper}>
+                <div className={styles.card} style={{
+                    backgroundRepeat:'no-repeat',
+                    backgroundImage: `url("${defaultImg}")`,
+                    backgroundPosition: 'center',
+                    backgroundSize: 'cover'
+                }} onClick={handleClick}>
+                <div className={styles.overlay}>
+                    <div>{title}</div>
+                </div>
+            </div>
         </div>
-    </div>
 }
 
 Cards.displayName = 'Cards';
